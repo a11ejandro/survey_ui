15356b79edb24ea11d2d59984778c120afa24e93
a11ejandro <bendthe@gmail.com>
2018-11-19 19:40:33 +0200

    Add tests for App component

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,63 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from '../App';
+import * as React from 'react'
+import toJson from 'enzyme-to-json'
+import { shallow } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import App from '../App'
+import * as Api from '../Api'
+
+jest.mock('../Api')
+jest.useFakeTimers()
+
+let mountedApp: any
+const app = () => {
+  if (!mountedApp) {
+    mountedApp = shallow(<App/>)
+  }
+  return mountedApp
+}
+
+it ('renders correctly', () => {
+  expect(toJson(app())).toMatchSnapshot()
+})
+
+it ('clears timeout on unmount', () => {
+  app().instance().componentWillUnmount()
+  expect(clearTimeout).toHaveBeenCalledTimes(1)
+})
+
+describe ('results', () => {
+  it ('is initialized as empty array', () => {
+    expect(app().state().results).toHaveLength(0)
+  })
+
+  it ('gets results from Api', async () => {
+    const spyOnGetStatistics = jest.spyOn(Api, 'getStatistics')
+    await app().instance().getResults()
+    expect(spyOnGetStatistics).toBeCalled()
+    expect(app().instance().state.results).toHaveLength(3)
+
+    spyOnGetStatistics.mockReset()
+    spyOnGetStatistics.mockRestore()
+  })
+})
+
+describe ('task creation', () => {
+  const newTask = {
+    page: 1,
+    per_page: 1000
+  }
+
+  it ('posts results to Api', async () => {
+    const spyOnCreateTask = jest.spyOn(Api, 'createTask')
+    await app().instance().createTask(newTask)
+    expect(spyOnCreateTask).toBeCalled()
+
+    spyOnCreateTask.mockReset()
+    spyOnCreateTask.mockRestore()
+  })
+
+  it ('delays result refresh', async () => {
+    await app().instance().createTask(newTask)
+    expect(setTimeout).toHaveBeenLastCalledWith(app().instance().getResults, 3000)
+  })
+})
