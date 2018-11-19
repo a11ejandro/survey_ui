dabf878e7f85c2cc4e3c31ec739187519e851ba3
a11ejandro <bendthe@gmail.com>
2018-11-19 19:37:26 +0200

    Implement StatisticsChart

--- src/StatisticsChart.tsx
+++ src/StatisticsChart.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
+import 'chartjs-plugin-datalabels'
 import { ChartData } from 'chart.js'
+import { HorizontalBar } from 'react-chartjs-2'
 
 import { Statistics } from './module'
 
@@ -11,8 +13,53 @@ interface State {
   chartData: ChartData
 }
 
+const chartOptions = {
+  plugins: {
+    datalabels: {
+      align: 'end',
+      anchor: 'end',
+      color: 'black'
+    }
+  }
+}
+
+
 export default class StatisticsChart extends React.Component<Props, State> {
+  public static getDerivedStateFromProps(newProps: Props, oldState: State) {
+    if (!newProps.data || newProps.data.length === 0 ) {
+      return { chartData: null }
+    }
+
+    const labels:string[] = newProps.data.map((sample: Statistics) => sample.handler_type)
+    const data:number[] = newProps.data.map((sample:Statistics) => sample.duration)
+
+    const dataset = {
+      label: newProps.data[0].collection_size + ' records',
+      data
+    }
+
+    return {
+      chartData: {
+        labels,
+        datasets: [dataset]
+      }
+    }
+    
+  }
+
+  constructor (props:Props) {
+    super(props)
+
+    this.state = {} as State
+  }
+
   public render () {
-    return null
+    if (!this.state.chartData) {
+      return null
+    }
+
+    return (
+      <HorizontalBar data={this.state.chartData} height={80} options={chartOptions}/>
+    )
   }
 }
