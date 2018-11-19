f016385528a75bec001163b9ddd99d44f6905eef
a11ejandro <bendthe@gmail.com>
2018-11-19 19:41:55 +0200

    Implement missing functionality in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
 
-import { statistics } from './__mocks__/Api'
 import { Statistics, Task } from './module'
+import { createTask, getStatistics } from './Api'
 import './App.css'
 import StatisticsChart from './StatisticsChart'
 import TaskForm from './TaskForm'
@@ -13,17 +14,20 @@ interface State {
 }
 
 class App extends React.Component<{}, State> {
+  private timeout: number
+
   constructor(props: {}) {
     super(props)
-
     this.state = {
       results: []
     }
-  
-    this.createTask = this.createTask.bind(this)
-    this.getResults = this.getResults.bind(this)
 
     this.createTask = this.createTask.bind(this)
+    this.getResults = this.getResults.bind(this)
+  }
+
+  public componentWillUnmount () {
+    clearTimeout(this.timeout)
   }
 
   public render() {
@@ -38,6 +42,15 @@ class App extends React.Component<{}, State> {
               <TaskForm createdCallback={this.createTask}/>
             </Grid>
             <Grid item xs={12}>
+              <Button
+                variant="outlined"
+                color="primary"
+                onClick={this.getResults}>
+                Get last results
+              </Button>
+            </Grid>
+            
+            <Grid item xs={12}>
               <StatisticsChart data={this.state.results}/>
             </Grid>
           </Grid>
@@ -47,16 +60,18 @@ class App extends React.Component<{}, State> {
   }
 
   private createTask (task: Task) {
-    console.log('submitted task')
-    this.getResults()
+    createTask(task).then((success: boolean) => {
+      setTimeout(this.getResults, 3000)
+    })
   }
 
   private getResults () {
-    this.setState({
-      results: statistics
+    getStatistics().then((data: Statistics[]) => {
+      this.setState({
+        results: data
+      })
     })
   }
 }
 
 export default App
-
