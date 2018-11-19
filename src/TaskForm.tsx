7abd234ad2254fc2f39354a27d724fcb031df340
a11ejandro <bendthe@gmail.com>
2018-11-19 18:22:43 +0200

    Implement TaskForm component

--- src/TaskForm.tsx
+++ src/TaskForm.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
-
+import TextField from '@material-ui/core/TextField'
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
 import { Task } from './module'
 
 interface State {
@@ -10,7 +12,65 @@ interface State {
 interface Props { createdCallback: (task: Task) => void }
 
 export default class TaskForm extends React.Component<Props, State> {
+  public constructor(props:Props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.submit = this.submit.bind(this)
+
+    this.state = {
+      page: 0,
+      per_page: 0
+    }
+  }
+
   public render () {
-    return null
+    const {page, per_page} = this.state
+    return (
+      <Grid container spacing={24}>
+        <Grid item xs={4}>
+          <TextField
+            id='page'
+            label="Page"
+            value={page}
+            onChange={this.handleChange('page')}
+            margin="normal"
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <TextField
+            id='per_page'
+            label="Per Page"
+            value={per_page}
+            onChange={this.handleChange('per_page')}
+            margin="normal"
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <br/>
+          <Button
+            id="submit_task"
+            variant="outlined"
+            color="primary"
+            onClick={this.submit}>
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
+    )
+  }
+
+  private handleChange = (name: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target as HTMLInputElement
+    this.setState((prevState) => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  private submit (event: React.SyntheticEvent) {
+    event.preventDefault()
+    const task = { page: this.state.page, per_page: this.state.per_page} as Task
+
+    this.props.createdCallback(task)
   }
 }
