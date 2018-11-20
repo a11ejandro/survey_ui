f3c9a8c6f98f71d1d89133b0b4827e5188798ade
a11ejandro <bendthe@gmail.com>
2018-11-20 02:44:31 +0200

    Supress requestAnimationFrame warnings on tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,12 @@
+import 'raf/polyfill'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+
+// Temporary hack to suppress error
+// https://github.com/facebookincubator/create-react-app/issues/3199
+window.requestAnimationFrame = function (callback) {
+  setTimeout(callback, 0)
+  return 0
+}
 
 Enzyme.configure({ adapter: new Adapter() })
